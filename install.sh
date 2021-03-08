28b01a985ea1f51f5e1240db7843c98efed0801b
isaiah-v <isaiah.v@comcast.net>
2021-03-08 07:47:46 +0000

    Changes to be committed: 	modified:   Nginx/nginx.conf 	modified:   install.sh

--- install.sh
+++ install.sh
@@ -16,30 +16,45 @@ fi
 sudo apt-get update
 sudo apt-get -y install apt-transport-https ca-certificates curl gnupg
 
-# Docker
+# --== Docker ==-- #
+
+#Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$DOCKER_ARCH signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
-# Docker Registry
+# Setup Docker Registry
 sudo docker run -d -p 5000:5000 --restart=always --name registry registry:2
 
-# Docker Registry UI
+# Setup Insecure Registry
+cp ./Docker/daemon.json ./daemon.json
+sed -i "s/<IP>/$PRIVATE_IP/" ./daemon.json
+sudo mv ./daemon.json /etc/docker/daemon.json
+sudo chown root:root /etc/docker/daemon.json
+
+# Setup Docker Registry UI
 sudo docker run --name docker-registry-ui -d -p 8081:80 --restart=always -e URL=https://ivcode.org -e REGISTRY_TITLE="ivcode.org" joxit/docker-registry-ui:static
 
-# Jenkins
-sudo docker run -d --restart=always -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) --name jenkins jenkins/jenkins
+# Restart Docker
+sudo systemctl restart docker
 
-# Nginx
+# --== Jenkins ==-- #
+sudo docker run -d -u root --restart=always -e JENKINS_OPTS="--prefix=/jenkins" -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) --name jenkins jenkins/jenkins:jdk11
+
+# --== Nginx ==-- #
 sudo docker run -d --restart=always -p 80:80 -p 443:443 --name nginx nginx
+
+# Setup Lets Encrypt SSL
 sudo docker exec nginx /bin/sh -c "apt-get update"
 sudo docker exec nginx /bin/sh -c "apt-get -y install certbot"
 sudo docker exec nginx /bin/sh -c "apt-get -y install python3-certbot-nginx"
 sudo docker exec nginx /bin/sh -c "certbot certonly --nginx --non-interactive --agree-tos -m isaiah.v@comcast.net -d ivcode.org"
 
+# Nginx Congif
 cp ./Nginx/nginx.conf ./nginx.conf
 sed -i "s/<IP>/$PRIVATE_IP/" ./nginx.conf
 sudo docker cp ./nginx.conf nginx:/etc/nginx/nginx.conf
 rm ./nginx.conf
 
+# Restart Nginx
 sudo docker restart nginx
