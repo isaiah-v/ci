ca126b5ccbe94d650b7cefc2baafe6dfa82aefed
Isaiah van der Elst <isaiah.v@comcast.net>
2021-03-14 13:48:41 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -27,6 +27,7 @@ sudo apt-get -y install apt-transport-https ca-certificates curl gnupg
 #Install Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$DOCKER_ARCH signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 # Setup Docker Registry
